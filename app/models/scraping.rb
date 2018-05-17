23aa262225e44e6ef14e89e6423bca0015e55d11
桑原亮介 <ryosuke0711993@gmail.com>
2018-05-17 13:02:19 +0900

    initial commit

---
+++ app/models/scraping.rb
@@ -0,0 +1,25 @@
+class Scraping
+  def self.movie_urls
+    links = []
+    agent = Mechanize.new
+    next_url = ""
+    current_page = agent.get("http://review-movie.herokuapp.com/" + next_url)
+    elements = current_page.search('.entry-title a')
+    elements.each do |ele|
+      links << ele.get_attribute('href')
+    end
+
+    links.each do |link|
+      get_product('http://review-movie.herokuapp.com/' + link)
+    end
+  end
+
+  def self.get_product(link)
+    agent = Mechanize.new
+    page = agent.get(link)
+    title = page.at('.entry-title').inner_text if page.at('.entry-title')
+    image_url = page.at('.entry-content img')[:src] if page.at('.entry-content img')
+    product = Product.where(title: title, image_url: image_url).first_or_initialize
+    product.save
+  end
+end
